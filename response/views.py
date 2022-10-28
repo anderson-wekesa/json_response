77db4a55eb622da3acdaf3855fd831cffc42b409
anderson-wekesa <110249466+anderson-wekesa@users.noreply.github.com>
2022-10-28 06:19:46 +0300

    Update views.py

--- response/views.py
+++ response/views.py
@@ -3,6 +3,8 @@ from .models import Me
 from .serializers import MeSerializer
 
 # Create your views here.
+#@api_view(['GET'])
 class AboutMe(generics.ListCreateAPIView):
     queryset = Me.objects.all()
     serializer_class = MeSerializer
+    http_method_names = ['get', 'options']
