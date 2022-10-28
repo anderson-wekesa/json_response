0fe5ddd17c1ad7082585bedaf49cff6443b1d32d
anderson-wekesa <110249466+anderson-wekesa@users.noreply.github.com>
2022-10-28 16:11:14 +0300

    Update urls.py

--- json_response/urls.py
+++ json_response/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api-auth/', include('rest_framework.urls')),
-    path('', views.AboutMe.as_view()),
-    path('about_me/', views.AboutMe.as_view()),
+    path('', views.AboutMe.home),
+    path('about_me/', views.AboutMe.home),
 ]
