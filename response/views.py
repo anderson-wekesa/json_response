31da551d2b881c003e6f865872b1486469ce66f5
anderson-wekesa <110249466+anderson-wekesa@users.noreply.github.com>
2022-10-28 16:10:00 +0300

    Update views.py

--- response/views.py
+++ response/views.py
@@ -1,10 +1,20 @@
-from rest_framework import generics
-from .models import Me
-from .serializers import MeSerializer
+#from rest_framework import generics
+#from .models import Me
+#from .serializers import MeSerializer
+
+#from rest_framework.decorators import api_view
+from django.http import JsonResponse
 
 # Create your views here.
-#@api_view(['GET'])
-class AboutMe(generics.ListCreateAPIView):
-    queryset = Me.objects.all()
-    serializer_class = MeSerializer
+class AboutMe():
     http_method_names = ['get', 'options']
+
+    def home(request, *args, **kwargs):
+      header = {"Access-Control-Allow-Origin":"*"}
+      data = {
+        "slackUsername":"callmeanderson",
+        "backend":True,
+        "age": 21,
+        "bio":"Django developer with a few months experience. I am a tech enthusiast and a rock lover."
+      }
+      return JsonResponse(data, headers=header)
